9f7cbc6aaa41fc4ecb44c2739ecd1dc0febc3114
nataliamoraluq <mora.luq.natalia68@gmail.com>
2025-02-12 11:12:10 -0400

    probando swagger (not working yet)

--- Web/Program.cs
+++ Web/Program.cs
@@ -14,38 +14,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//examen 1 pregunta B - swagger y como implementarlo - natalia ml
-//Program.cs field > Web project
 
-builder.Services.AddSwaggerGen(options =>{
+builder.Services.AddSwaggerGen(options =>
+{
     options.SwaggerDoc("v1", new OpenApiInfo
     {
         //info a presentar en la doc.
         Version = "v1",
-        Title = "API rpg to do",
-        Description = "An ASP.NET Core Web API for an rpg game",
+        Title = "API GracoSoft .NET CORE 2025",
+        Description = "An App Web API for an rpg game usin ASP.NET",
         TermsOfService = new Uri("https://example.com/terms"),
         Contact = new OpenApiContact
         {
-            Name = "Example Contact",Url = new Uri("https://example.com/contact")
+            Name = "Guilermo Gimenez (prof) - IV Cohorte; Natalia Mora ",
+            Url = new Uri("https://github.com/G3-Graco/entregable-2-nataliamoraluq.git")
         },
         License = new OpenApiLicense
         {
-            Name = "Example License",Url = new Uri("https://example.com/license")
+            Name = "Graco RPG API - License",
+            Url = new Uri("https://example.com/license")
         }
     });
 
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-    
+
 });
-
-//Program.cs field > Web project
-
-
-
-
 
 builder.Services.AddControllers();
 
@@ -78,8 +72,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 
 app.Run();
 
